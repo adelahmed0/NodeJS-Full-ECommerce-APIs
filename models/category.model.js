5119a8baf2b64196a81557dbf261a027ca06cecd
Adel Ahmed <adel15952@gmail.com>
2026-02-05 01:37:06 +0200

    feat: add Mongoose Category model with name, slug, and image fields

--- models/category.model.js
+++ models/category.model.js
@@ -1,21 +1,26 @@
 import mongoose from "mongoose";
 
-const categorySchema = new mongoose.Schema({
+const categorySchema = new mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: [true, "Category name is required"],
-        trim: true,
-        unique: [true, "Category name already exists"],
-        minLength: [3, "Category name must be at least 3 characters long"],
-        maxLength: [32, "Category name must be at most 50 characters long"]
+      type: String,
+      required: [true, "Category name is required"],
+      trim: true,
+      unique: [true, "Category name already exists"],
+      minLength: [3, "Category name must be at least 3 characters long"],
+      maxLength: [32, "Category name must be at most 50 characters long"],
     },
     slug: {
-        type: String,
-        lowercase:true
+      type: String,
+      lowercase: true,
     },
-},{timestamps:true});
+    image: {
+      type: String,
+    },
+  },
+  { timestamps: true },
+);
 
 const Category = mongoose.model("Category", categorySchema);
 
 export default Category;
-
