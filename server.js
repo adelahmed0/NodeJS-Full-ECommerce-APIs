b08d5fef201f1f752b5c0ec3cb9b23350559b587
Adel Ahmed <adel15952@gmail.com>
2026-02-04 23:52:07 +0200

    feat: Add Morgan for HTTP request logging in development environment.

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
+import morgan from "morgan";
 
 
 dotenv.config();
 
 const app = express();
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 
 const PORT = process.env.PORT || 8000;
 
