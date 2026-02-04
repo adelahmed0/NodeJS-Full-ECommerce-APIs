4a01127a6e4869ad6ec44bd1556e82aba294f2c2
Adel Ahmed <adel15952@gmail.com>
2026-02-05 01:05:03 +0200

    feat: Add Mongoose for MongoDB integration and establish database connection.

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import morgan from "morgan";
-
+import mongoose from "mongoose";
 
 dotenv.config();
 
@@ -10,6 +10,18 @@ const app = express();
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
+
+const MONGO_URI = process.env.MONGO_URI;
+
+mongoose
+  .connect(MONGO_URI)
+  .then((connection) => {
+    console.log(`MongoDB connected ${connection.connection.host}`);
+  })
+  .catch((error) => {
+    console.log(`Database connection error: ${error}`);
+    process.exit(1);
+  });
 
 const PORT = process.env.PORT || 8000;
 
