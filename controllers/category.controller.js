883e20980a68e2bc97c989791655341ee0bb7675
Adel Ahmed <adel15952@gmail.com>
2026-02-05 14:18:14 +0200

    feat: add category creation and retrieval API endpoints and services

--- controllers/category.controller.js
+++ controllers/category.controller.js
@@ -1,5 +1,5 @@
 import asyncHandler from "express-async-handler";
-import { createCategoryService } from "../services/category.service.js";
+import { createCategoryService,getAllCategoriesService } from "../services/category.service.js";
 
 /**
  * @desc    Create category
@@ -10,4 +10,14 @@ export const createCategory = asyncHandler(async (req, res) => {
   const { name } = req.body;
   const category = await createCategoryService(name);
   res.status(201).json({ data: category });
+});
+
+/**
+ * @desc    Get all categories
+ * @route   GET /api/categories
+ * @access  Public
+ */
+export const getAllCategories = asyncHandler(async (req, res) => {
+  const categories = await getAllCategoriesService();
+  res.status(200).json({ data: categories,results:categories.length });
 });
--- services/category.service.js
+++ services/category.service.js
@@ -1,8 +1,15 @@
 import Category from "../models/category.model.js";
 import slugify from "slugify";
 
+
 export const createCategoryService = async (name) => {
   const slug = slugify(name, { lower: true });
   const category = await Category.create({ name, slug });
   return category;
+};
+
+
+export const getAllCategoriesService = async () => {
+  const categories = await Category.find();
+  return categories;
 };
