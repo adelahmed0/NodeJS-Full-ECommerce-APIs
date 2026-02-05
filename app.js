716439c5a22ed56a2b96d6e2dd1c7fcfa9e41ec9
Adel Ahmed <adel15952@gmail.com>
2026-02-05 14:31:07 +0200

    refactor: Restructure server into `app.js` and `index.js` files, incorporating new security, rate limiting, and CORS middleware.

---
+++ app.js
@@ -0,0 +1,43 @@
+import express from "express";
+import helmet from "helmet";
+import { rateLimit } from "express-rate-limit";
+import cors from "cors";
+import morgan from "morgan";
+
+import categoryRouter from "./routes/category.route.js";
+
+const app = express();
+const api = process.env.API_PREFIX || "/api";
+
+app.use(helmet());
+
+// Global Rate Limiting
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  standardHeaders: "draft-7", // set `RateLimit` and `RateLimit-Policy` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  message: "Too many requests, please try again later.",
+});
+
+app.use(`${api}`, limiter);
+
+app.use(express.json());
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization", "Accept-Language"],
+  }),
+);
+
+// Routes
+app.use(`${api}/categories`, categoryRouter);
+
+export default app;
---
+++ index.js
@@ -0,0 +1,14 @@
+import dotenv from "dotenv";
+import app from "./app.js";
+import connectDB from "./config/db.js";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 8000;
+
+// Connect to Database
+connectDB();
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
