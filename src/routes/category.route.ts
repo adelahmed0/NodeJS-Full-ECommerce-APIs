ed43b29e6900726f111a1f18cf12b6c72a4fa6b2
Adel Ahmed <adel15952@gmail.com>
2026-02-06 23:32:38 +0200

    feat: Add category routes and validation for CRUD operations.

--- src/routes/category.route.ts
+++ src/routes/category.route.ts
@@ -6,14 +6,19 @@ import {
   updateCategory,
   deleteCategory,
 } from "../controllers/category.controller.js";
-import { getCategoryByIdValidator } from "../validators/category.validator.js";
+import {
+  getCategoryByIdValidator,
+  createCategoryValidator,
+  updateCategoryValidator,
+  deleteCategoryValidator,
+} from "../validators/category.validator.js";
 
 const router: Router = express.Router();
 
-router.post("/", createCategory);
+router.post("/", createCategoryValidator, createCategory);
 router.get("/", getAllCategories);
 router.get("/:id", getCategoryByIdValidator, getCategoryById);
-router.put("/:id", updateCategory);
-router.delete("/:id", deleteCategory);
+router.put("/:id", updateCategoryValidator, updateCategory);
+router.delete("/:id", deleteCategoryValidator, deleteCategory);
 
 export default router;
--- src/validators/category.validator.ts
+++ src/validators/category.validator.ts
@@ -1,6 +1,34 @@
 import { body, param, query, validationResult } from "express-validator";
 import validatorMiddleware from "../middleware/validator.middleware.js";
+
 export const getCategoryByIdValidator = [
+  param("id").isMongoId().withMessage("Invalid Category ID"),
+  validatorMiddleware,
+];
+
+export const createCategoryValidator = [
+  body("name")
+    .notEmpty()
+    .withMessage("Category name is required")
+    .isLength({ min: 3 })
+    .withMessage("Too short category name")
+    .isLength({ max: 32 })
+    .withMessage("Too long category name"),
+  validatorMiddleware,
+];
+
+export const updateCategoryValidator = [
+  param("id").isMongoId().withMessage("Invalid Category ID"),
+  body("name")
+    .optional()
+    .isLength({ min: 3 })
+    .withMessage("Too short category name")
+    .isLength({ max: 32 })
+    .withMessage("Too long category name"),
+  validatorMiddleware,
+];
+
+export const deleteCategoryValidator = [
   param("id").isMongoId().withMessage("Invalid Category ID"),
   validatorMiddleware,
 ];
