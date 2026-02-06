0d41089a460f8327bde9b025c0e36eb5336dedd0
Adel Ahmed <adel15952@gmail.com>
2026-02-06 23:29:27 +0200

    feat: Add category routes with ID validation.

--- src/validators/category.validator.ts
+++ src/validators/category.validator.ts
@@ -1,5 +1,6 @@
 import { body, param, query, validationResult } from "express-validator";
-
+import validatorMiddleware from "../middleware/validator.middleware.js";
 export const getCategoryByIdValidator = [
   param("id").isMongoId().withMessage("Invalid Category ID"),
+  validatorMiddleware,
 ];
