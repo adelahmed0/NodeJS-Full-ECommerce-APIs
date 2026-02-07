6c0fdf85d8e072851d10709d4fd62d457f5b1ab4
Adel Ahmed <adel15952@gmail.com>
2026-02-08 01:12:36 +0200

    feat: implement product service with CRUD operations, pagination, and slug generation.

---
+++ src/services/product.service.ts
@@ -0,0 +1,91 @@
+import Product, { IProduct } from "../models/product.model.js";
+import slugify from "@sindresorhus/slugify";
+import { IAllProductsResponse } from "../types/product.types.js";
+
+/**
+ * Create a new product
+ */
+export const createProductService = async (
+  productData: Partial<IProduct>,
+): Promise<IProduct> => {
+  if (productData.title) {
+    productData.slug = slugify(productData.title, { lowercase: true });
+  }
+  const product = await Product.create(productData);
+  return product;
+};
+
+/**
+ * Get all products with pagination and filter
+ */
+export const getAllProductsService = async (
+  page: number,
+  per_page: number,
+  filter: any = {},
+): Promise<IAllProductsResponse> => {
+  const skip = (page - 1) * per_page;
+  const products = await Product.find(filter)
+    .populate({ path: "category", select: "name image" })
+    .populate({ path: "brand", select: "name image" })
+    .populate({ path: "subcategories", select: "name" })
+    .skip(skip)
+    .limit(per_page)
+    .sort("-createdAt"); // Newest products first
+
+  const totalProducts = await Product.countDocuments(filter);
+  const totalPages = Math.ceil(totalProducts / per_page);
+
+  return {
+    products,
+    pagination: {
+      total_count: totalProducts,
+      current_page: page,
+      last_page: totalPages,
+      per_page: per_page,
+    },
+  };
+};
+
+/**
+ * Get product by ID
+ */
+export const getProductByIdService = async (
+  id: string,
+): Promise<IProduct | null> => {
+  const product = await Product.findById(id)
+    .populate({ path: "category", select: "name image" })
+    .populate({ path: "brand", select: "name image" })
+    .populate({ path: "subcategories", select: "name" });
+  return product;
+};
+
+/**
+ * Update product by ID
+ */
+export const updateProductService = async (
+  id: string,
+  updateData: Partial<IProduct>,
+): Promise<IProduct | null> => {
+  if (updateData.title) {
+    updateData.slug = slugify(updateData.title, { lowercase: true });
+  }
+  const product = await Product.findByIdAndUpdate(id, updateData, {
+    new: true,
+    runValidators: true,
+  })
+    .populate({ path: "category", select: "name image" })
+    .populate({ path: "brand", select: "name image" })
+    .populate({ path: "subcategories", select: "name" });
+
+  return product;
+};
+
+/**
+ * Delete product by ID
+ */
+export const deleteProductService = async (
+  id: string,
+): Promise<IProduct | null> => {
+  const product = await Product.findByIdAndDelete(id);
+  return product;
+};
