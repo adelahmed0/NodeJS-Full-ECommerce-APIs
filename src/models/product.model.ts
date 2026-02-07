538218cd05634b43914c4a490ea5d5ca494f3ac3
Adel Ahmed <adel15952@gmail.com>
2026-02-08 00:56:35 +0200

    feat: create product Mongoose schema and model with validation and population.

---
+++ src/models/product.model.ts
@@ -0,0 +1,106 @@
+import mongoose, { Schema, Document, Types } from "mongoose";
+import { toJSONPlugin } from "../helpers/mongoosePlugins.js";
+
+interface IProduct extends Document {
+  title: string;
+  slug: string;
+  description: string;
+  quantity: number;
+  sold: number;
+  price: number;
+  priceAfterDiscount: number;
+  colors: string[];
+  imageCover: string;
+  images: string[];
+  category: Types.ObjectId;
+  subcategories: Types.ObjectId[];
+  brand: Types.ObjectId;
+  ratingsAverage: number;
+  ratingsQuantity: number;
+}
+const productSchema = new Schema<IProduct>(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: [3, "Too short product title"],
+      maxlength: [100, "Too long product title"],
+    },
+    slug: {
+      type: String,
+      required: true,
+      lowercase: true,
+    },
+    description: {
+      type: String,
+      required: [true, "Product description is required"],
+      minlength: [20, "Too short product description"],
+    },
+    quantity: {
+      type: Number,
+      required: [true, "Product quantity is required"],
+    },
+    sold: {
+      type: Number,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: [true, "Product price is required"],
+      trim: true,
+      max: [200000, "Too long product price"],
+    },
+    priceAfterDiscount: {
+      type: Number,
+    },
+    colors: [String],
+
+    imageCover: {
+      type: String,
+      required: [true, "Product Image cover is required"],
+    },
+    images: [String],
+    category: {
+      type: Types.ObjectId,
+      ref: "Category",
+      required: [true, "Product must be belong to category"],
+    },
+    subcategories: [
+      {
+        type: Types.ObjectId,
+        ref: "SubCategory",
+      },
+    ],
+    brand: {
+      type: Types.ObjectId,
+      ref: "Brand",
+    },
+    ratingsAverage: {
+      type: Number,
+      min: [1, "Rating must be above or equal 1.0"],
+      max: [5, "Rating must be below or equal 5.0"],
+    },
+    ratingsQuantity: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true },
+);
+
+// Mongoose query middleware
+productSchema.pre(
+  /^find/,
+  function (this: mongoose.Query<any, IProduct>, next) {
+    this.populate({
+      path: "category",
+      select: "name -_id",
+    });
+    next();
+  },
+);
+
+productSchema.plugin(toJSONPlugin);
+
+export default mongoose.model("Product", productSchema);
