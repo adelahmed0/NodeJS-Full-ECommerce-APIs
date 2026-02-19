e6ef4e551babd632b81ecee2e2b9014497f8f17b
Adel Ahmed <adel15952@gmail.com>
2026-02-20 01:56:47 +0200

    feat: Add category routes for CRUD operations, including image upload with unique filenames.

--- src/routes/category.route.ts
+++ src/routes/category.route.ts
@@ -16,9 +16,20 @@ import {
 import subCategoryRouter from "./subCategory.route.js";
 import multer from "multer";
 import { Request, Response, NextFunction } from "express";
+import { v4 as uuidv4 } from "uuid";
+
+
+const multerStorage = multer.diskStorage({
+  destination: "uploads/categories",
+  filename: (req, file, cb) => {
+    const ext = file.mimetype.split("/")[1];
+    const fileName = `category-${uuidv4()}-${Date.now()}.${ext}`;
+    cb(null, fileName);
+  },
+});
 
 const upload = multer({
-  dest: "uploads/categories",
+  storage: multerStorage
 });
 
 const router: Router = express.Router();
