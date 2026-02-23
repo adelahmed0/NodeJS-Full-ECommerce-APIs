35650db949638fa132fe5a30951dc3e98b02297a
Adel Ahmed <adel15952@gmail.com>
2026-02-23 19:04:19 +0200

    feat: Implement global error handling middleware and user routes with CRUD operations, password change, validation, and avatar upload.

--- src/middleware/globalError.middleware.ts
+++ src/middleware/globalError.middleware.ts
@@ -1,4 +1,5 @@
 import { Request, Response, NextFunction } from "express";
+import { ApiError } from "../utils/apiError.js";
 
 const globalError = (
   err: any,
@@ -9,12 +10,21 @@ const globalError = (
   err.statusCode = err.statusCode || 500;
   err.status = err.status || false;
 
+  if (err.name === "JsonWebTokenError") err = handleJwtInvalidSignature();
+  if (err.name === "TokenExpiredError") err = handleJwtExpired();
+
   if (process.env.NODE_ENV === "development") {
     sendErrorForDev(err, res);
   } else {
     sendErrorForProd(err, res);
   }
 };
+
+const handleJwtInvalidSignature = () =>
+  new ApiError("Invalid session. Please log in again.", 401);
+
+const handleJwtExpired = () =>
+  new ApiError("Your session has expired. Please log in again.", 401);
 
 const sendErrorForDev = (err: any, res: Response) =>
   res.status(err.statusCode).json({
