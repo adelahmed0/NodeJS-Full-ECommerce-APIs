a221cf46c04a6ee4bd5135fb0c747addc3da715c
Adel Ahmed <adel15952@gmail.com>
2026-02-23 02:00:12 +0200

    feat: implement initial signup and login services for user authentication.

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,4 +1,6 @@
+import { NextFunction } from "express";
 import User, { IUser } from "../models/user.model.js";
+import { ApiError } from "../utils/apiError.js";
 import { createToken } from "../utils/token.js";
 
 // @desc    Signup
@@ -12,4 +14,19 @@ export const signupService = async (body: IUser) => {
     type: user.type,
   });
   return { user, token };
+};
+
+// @desc    Login
+// @route   POST /api/auth/login
+// @access  Public
+export const loginService = async (body: IUser, next: NextFunction) => {
+  // 1-check if password and email are provided
+  // 2-check if user exists
+  const user = await User.findOne({ email: body.email });
+  if (!user) {
+    return next(new ApiError("Invalid email or password", 400));
+  }
+  // 3-check if password is correct
+  // 4-create token
+  // 5-return user and token
 };
