d7914ab2d296e362230ecdbc7c534e5ef24b6c11
Adel Ahmed <adel15952@gmail.com>
2026-02-24 20:59:15 +0200

    feat: Implement email sending utility using Nodemailer.

---
+++ src/utils/sendEmail.ts
@@ -0,0 +1,14 @@
+import nodemailer from "nodemailer";
+/**
+ * Utility to send emails using nodemailer
+ */
+const sendEmail = async (options) => {
+  // 1) Create transporter (service like gmail, mailtrap, etc.)
+
+
+  //   2) Define email options
+
+  //   3) Send email
+};
+
+export default sendEmail;
